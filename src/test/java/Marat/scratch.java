aa7a8be436d354a230cc0185cf0b8cacd3283a1a
mrDiabllo <51274074+mrDiabllo@users.noreply.github.com>
2019-06-03 14:37:37 -0500

    Update scratch.java

--- src/test/java/Marat/scratch.java
+++ src/test/java/Marat/scratch.java
@@ -1,5 +1,6 @@
 class Scratch {
     public static void main(String[] args) {
         System.out.println("I dont know what is the scratch.");
+        System.out.println("Now you know it"); 
     }
 }
