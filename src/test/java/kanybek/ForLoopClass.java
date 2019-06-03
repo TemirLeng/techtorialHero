ade6b9d28e4ed72fef213de0b432bf5351d42c7e
Kanybek Zhagusaev <zhagusaev@gmail.com>
2019-06-03 01:46:08 -0500

    for loop file is pushed

---
+++ src/test/java/kanybek/ForLoopClass.java
@@ -0,0 +1,11 @@
+package kanybek;
+
+public class ForLoopClass {
+
+    public static void main(String[] args) {
+        int [] arr = {1,4,4,5,6,34,};
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
