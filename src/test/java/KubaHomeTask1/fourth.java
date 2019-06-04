677eb0932fde6429738ba73a1670678dbb09b75e
Alisha2000 <amely1988@bk.ru>
2019-06-04 17:10:34 -0500

    my changes

---
+++ src/test/java/KubaHomeTask1/fourth.java
@@ -0,0 +1,11 @@
+package KubaHomeTask1;
+
+public class fourth {
+
+    public static void main(String[] args) {
+
+        System.out.println("Fourth class");
+
+    }
+
+}
---
+++ src/test/java/kanybek/HomeWork/Class.java
@@ -0,0 +1,7 @@
+package kanybek.HomeWork;
+
+public class Class {
+    public static void main(String[] args) {
+        System.out.println("I'm so good at sleeping. I can do it with my eyes closed.");
+    }
+}
